e217d8a637601ea833897be76afa840423e41739
WizyWoo <kevin.hagfors@gmail.com>
2020-10-08 15:15:45 +0200

    s

--- Assets/_Scripts/EnemyHealth.cs
+++ Assets/_Scripts/EnemyHealth.cs
@@ -11,38 +11,39 @@ public class EnemyHealth : MonoBehaviour
     public GameObject Zombie;
     public CuredZombie CuredZ;
     public CuredHuman CuredH;
-    public SpriteRenderer spriteRenderer;
+    public SwitchTargetDestination targetSwitch;
+    public SpriteRenderer SpriteRenderer;
     public GameObject zombieSprite;
-    public AIDestinationSetter AiDestination1;
-    public AIDestinationSetter AiDestination2;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        spriteRenderer.sprite = EnemyFullHealth[enemyhealthbar];
+        transform.GetComponent<SpriteRenderer>().sprite = EnemyFullHealth[enemyhealthbar];
+        SpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
     }
 
     public void LowerHealth()
     {
         //Removes 1 health from the zombie and turns on the health sprite
         enemyhealthbar--;
-        spriteRenderer.enabled = true;
+        SpriteRenderer.enabled = true;
 
         //Checks if the zombie has health left, if not it destroys the zombie
         if (enemyhealthbar >= 1)
         {
-            spriteRenderer.sprite = EnemyFullHealth[enemyhealthbar];
+            transform.GetComponent<SpriteRenderer>().sprite = EnemyFullHealth[enemyhealthbar];
         }
         else
         {
 
             Destroy(zombieSprite);
-            spriteRenderer.enabled = false;
+            SpriteRenderer.enabled = false;
             Zombie.GetComponentInChildren<CuredZombie>().CuredZ();
             Zombie.GetComponentInChildren<CuredHuman>().CuredH();
-            AiDestination1.enabled = false;
-            AiDestination2.enabled = true;
+            Zombie.GetComponent<SwitchTargetDestination>().SwitchTarget();
+
 
         }
 
