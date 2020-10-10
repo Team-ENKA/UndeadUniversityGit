240f8d0b040e271b6a7a22b73e901ee02ed3a0da
appelsapsap <kate.m.sarkol@student.nord.no>
2020-10-10 16:47:08 +0200

    Lunchlass takes damage and dissapears when dead, using new health slider

--- Assets/BoHealthController.cs
+++ Assets/BoHealthController.cs
@@ -1,12 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BoHealthController : MonoBehaviour
 {
     public int maxHealth = 30;
     public int currentHealth;
     public BoHealthBar healthBar;
+
+    public GameObject LunchLass;
+    public float playerInvincibility;
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && playerInvincibility <= 0)
+        {
+            //healthBar.LowerHealth();
+            playerInvincibility = 1;
+            TakeDamage(5);
+        }
+    }
+    
+    /*void OnCollisionStay2D(Collider2D collision)
+    {
+        if (gameObject.collision.tag == "Enemy")
+        {
+            TakeDamage(5);
+        }
+    }*/
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +40,17 @@ public class BoHealthController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            TakeDamage(5);
-        }
+         playerInvincibility = playerInvincibility - Time.deltaTime;
     }
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
+        
+        if (currentHealth == 0)
+        {
+            Destroy(LunchLass);
+        }
     }
 }
