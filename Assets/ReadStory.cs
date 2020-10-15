fde80d1d276ab9bfe40cf920a9aa286b8d67a034
NohrMann <345982@student.nord.no>
2020-10-15 15:07:29 +0200

    Added book for story

---
+++ Assets/ReadStory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ReadStory : MonoBehaviour
+{
+    public MeshRenderer textToShow;
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (Input.GetKey("e") && collision.gameObject.tag == "Player")
+        {
+            textToShow.enabled = true;
+
+        }
+
+    }
+
+}
