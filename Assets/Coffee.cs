6754554efa545284a59d3fb487ff86d1b26a1dd0
NohrMann <72544317+NohrMann@users.noreply.github.com>
2020-10-16 10:00:05 +0200

    Coffee Prefab

---
+++ Assets/Coffee.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coffee : MonoBehaviour
+{
+    public Movement Movement;
+
+
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (Input.GetKey("e") && collision.gameObject.tag == "Player")
+        {
+
+            Destroy(gameObject);
+            Movement.CoffeeBoost();
+
+        }
+
+    }
+
+}
