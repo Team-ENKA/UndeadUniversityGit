fea087bbf87bebef669128a4af7fae18f513cfc8
WizyWoo <kevin.hagfors@gmail.com>
2020-11-03 17:19:00 +0100

    PlayerDetection

---
+++ Assets/_Scripts/Enemies/PlayerDetection.cs
@@ -0,0 +1,27 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+
+    public Vector2 scanOrigin;
+    public Transform zombieScanTransform;
+    public Transform playerPos;
+    public AIDestinationSetter Destination;
+    int layerMask = 13;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        scanOrigin = new Vector2(zombieScanTransform.position.x, zombieScanTransform.position.y);
+        RaycastHit2D hit = Physics2D.Linecast(scanOrigin, playerPos.position, layerMask);
+        Debug.DrawLine(scanOrigin, playerPos.position, Color.green, 2f);
+
+        if (hit.collider.gameObject.tag == "Player")
+            Debug.Log("rawr");
+
+    }
+}
