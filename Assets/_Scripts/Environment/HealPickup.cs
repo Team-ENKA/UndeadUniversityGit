b8c663e54aaa1169a914c4ff79e7525804f88dc6
NohrMann <72544317+NohrMann@users.noreply.github.com>
2020-11-03 18:49:29 +0100

    Functional Heal Pickup and store

--- Assets/_Scripts/Environment/HealPickup.cs
+++ Assets/_Scripts/Environment/HealPickup.cs
@@ -9,22 +9,18 @@ public class HealPickup : MonoBehaviour
     public HealBoxAmount healBoxAmount;
     public int healAmount;
 
-    public void Heal()
-    {
-        health.currentHealth += healAmount;
-        healthBar.SetHealth(health.currentHealth);
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && health.currentHealth < 30)
         {
-            Heal();
+            healBoxAmount.Heal();
             Destroy(gameObject);
         }
         if (collision.gameObject.tag == "Player" && health.currentHealth == 30)
         {
             healBoxAmount.AddHealBox();
+            Destroy(gameObject);
         }
     }
 
