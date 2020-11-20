21549a9f6d61f562a3bac594e4e7d2a1dd70d4f8
WizyWoo <kevin.hagfors@gmail.com>
2020-11-20 16:52:49 +0100

    some sounds

--- Assets/_Scripts/Player/BoHealthController.cs
+++ Assets/_Scripts/Player/BoHealthController.cs
@@ -20,6 +20,9 @@ public class BoHealthController : MonoBehaviour
 
     public HealBoxAmount HealBoxAmount;
 
+    public PlaySound playSound;
+    public GameObject pS;
+
     void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy" && playerInvincibility <= 0)
@@ -29,18 +32,12 @@ public class BoHealthController : MonoBehaviour
             TakeDamage(5);
         }
     }
-    
-    /*void OnCollisionStay2D(Collider2D collision)
-    {
-        if (gameObject.collision.tag == "Enemy")
-        {
-            TakeDamage(5);
-        }
-    }*/
 
     // Start is called before the first frame update
     void Start()
     {
+        pS = GameObject.FindGameObjectWithTag("PlayerSFX");
+        playSound = pS.GetComponent<PlaySound>();
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
     }
@@ -67,10 +64,13 @@ public class BoHealthController : MonoBehaviour
         
         if (currentHealth == 0)
         {
+
             for (int i = 0; i < deathEffectParticles; i++)
                 Instantiate(deathParticle, transform.position, Quaternion.identity);
             pause.active = true;
+            playSound.PlayerDied();
             Destroy(LunchLass);
+
         }
     }
 }
