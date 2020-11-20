750e763c4f4afa9c5c49aca8940c1938c27b4023
ENKA TeamLead <345982@student.nord.no>
2020-11-20 16:59:18 +0100

    HtoHeal

---
+++ Assets/_Scripts/UI/ShowHHeal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowHHeal : MonoBehaviour
+{
+
+    public BoHealthController health;
+    public Text HtoHealText;
+    private void Update()
+    {
+        if(health.currentHealth < 15)
+        {
+            HtoHealText.;
+        }
+        if(health.currentHealth > 15)
+        {
+            HtoHealText.SetActive(false);
+        }
+    }
+}
