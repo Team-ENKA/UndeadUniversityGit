8fce3c66b710e1bdc18a25abe98091254f735010
sketchyyu <juliastokke@gmail.com>
2020-11-27 20:51:43 +0100

    Revert "Merge branch 'master' of https://github.com/Team-ENKA/UndeadUniversityGit"
    This reverts commit 6ec29cdccf90b9dc164aa08701fe7666fbb98697, reversing
    changes made to 1ed16dea2d07ced69c7b1fe0cca495c4f5c23cca.

---
+++ Assets/_Scripts/Environment/Lever2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Lever2 : MonoBehaviour
+{
+
+    public Transform doorTransform;
+    public int requiredCures = 18;
+    public GameObject requirementCanvas;
+    public string have;
+    public string need;
+    public TextMeshPro counterVsReq;
+    public SpriteRenderer upSprite;
+    public SpriteRenderer downSprite;
+
+    private GameObject cureCounter;
+    private CureCounter cureCounterScript;
+    private int cd = 0;
+    private float counter = 0;
+
+    public void Start()
+    {
+        cureCounter = GameObject.FindGameObjectWithTag("CureCounter");
+        cureCounterScript = cureCounter.GetComponent<CureCounter>();
+        requirementCanvas.SetActive(false);
+        downSprite.enabled = false;
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && cd == 0)
+        {
+            if (cureCounterScript.counter < requiredCures)
+            {
+                NotEnough();
+            }
+            if (cureCounterScript.counter >= requiredCures)
+            {
+                Debug.Log("Lever Pull");
+                cd++;                                                               //Increase cooldown so door opens only once
+                float rotateDoorTo = doorTransform.rotation.eulerAngles.z + 90f;    //Rotation amount (90 degrees)
+                doorTransform.rotation = Quaternion.Euler(0, 0, rotateDoorTo);      //Rotate door along z axis
+                downSprite.enabled = true;                                          //Flips the switch downwards
+                upSprite.enabled = false;                                           //Disable current sprite
+            }
+        }
+    }
+    private void NotEnough()
+    {
+        counter = 3;
+        requirementCanvas.SetActive(true);
+        need = requiredCures.ToString();                        //need = predefined requirement, here 18
+        have = cureCounterScript.counter.ToString();            //have = current amount of cures
+        counterVsReq.text = have + " / " + need;                //have cured / need cured
+   
+        Debug.Log("Cure more zombies, the door is locked");
+        /*
+         * Play sound Gadunk
+        */
+    }
+    private void Update()
+    {
+        counter -= Time.deltaTime;
+        if (counter <= 0)
+        requirementCanvas.SetActive(false);
+    }
+}
