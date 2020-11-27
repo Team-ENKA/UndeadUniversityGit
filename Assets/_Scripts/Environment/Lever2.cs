826e190f3688ad42c56bbea7f743dbfb84fc333e
ENKA TeamLead <345982@student.nord.no>
2020-11-27 14:07:58 +0100

    Lever2

--- Assets/_Scripts/Environment/Lever2.cs
+++ Assets/_Scripts/Environment/Lever2.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Lever2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Transform doorTransform;
+
+    private GameObject cureCounter;
+    private CureCounter cureCounterScript;
+    private int cd = 0;
+
+    public void Start()
     {
-        
+        cureCounter = GameObject.FindGameObjectWithTag("CureCounter");
+        cureCounterScript = cureCounter.GetComponent<CureCounter>();
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && cd == 0)
+        {
+            if (cureCounterScript.counter < 18)
+            {
+                Debug.Log("Cure more zombies, the door is locked");
+            }
+            if (cureCounterScript.counter >= 18)
+            {
+                Debug.Log("Lever Pull");
+                cd++;                                                               //Increase cooldown so door opens only once
+                float rotateDoorTo = doorTransform.rotation.eulerAngles.z + 90f;    //Rotation amount (90 degrees)
+                doorTransform.rotation = Quaternion.Euler(0, 0, rotateDoorTo);      //Rotate door along z axis
+            }
+        }
     }
 }
