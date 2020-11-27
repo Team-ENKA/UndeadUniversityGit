642dfb8ba2a4393c4ed9f35afa6f71db8778a8e9
ENKA TeamLead <345982@student.nord.no>
2020-11-27 15:21:21 +0100

    Lever2 to display requirement

--- Assets/_Scripts/Environment/Lever2.cs
+++ Assets/_Scripts/Environment/Lever2.cs
@@ -9,7 +9,7 @@ public class Lever2 : MonoBehaviour
 
     public Transform doorTransform;
     public int requiredCures = 18;
-    public Canvas requirement;
+    public GameObject requirementCanvas;
     public string have;
     public string need;
     public TextMeshPro counterVsReq;
@@ -17,11 +17,13 @@ public class Lever2 : MonoBehaviour
     private GameObject cureCounter;
     private CureCounter cureCounterScript;
     private int cd = 0;
+    private float counter = 0;
 
     public void Start()
     {
         cureCounter = GameObject.FindGameObjectWithTag("CureCounter");
         cureCounterScript = cureCounter.GetComponent<CureCounter>();
+        requirementCanvas.SetActive(false);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -29,20 +31,7 @@ public class Lever2 : MonoBehaviour
         {
             if (cureCounterScript.counter < requiredCures)
             {
-                need = requiredCures.ToString();                        //need = predefined requirement, here 18
-                have = cureCounterScript.counter.ToString();            //have = current amount of cures
-                counterVsReq.text = have + " / " + need;                //have cured / need cured
-                Debug.Log("Cure more zombies, the door is locked");
-                /*
-                 * counter = 3
-                 * counter -= Time.Deltatime
-                 * Change sprite from Up to Down
-                 * show image Gingerbreadman
-                 * show text, which displays zombies cured VS required
-                 * Play sound Gadunk
-                 * if (counter <= 0)
-                 * disable 
-                */
+                NotEnough();
             }
             if (cureCounterScript.counter >= requiredCures)
             {
@@ -51,6 +40,26 @@ public class Lever2 : MonoBehaviour
                 float rotateDoorTo = doorTransform.rotation.eulerAngles.z + 90f;    //Rotation amount (90 degrees)
                 doorTransform.rotation = Quaternion.Euler(0, 0, rotateDoorTo);      //Rotate door along z axis
             }
+        }
+    }
+    private void NotEnough()
+    {
+        counter = 3;
+        counter -= Time.deltaTime;
+        requirementCanvas.SetActive(true);
+        need = requiredCures.ToString();                        //need = predefined requirement, here 18
+        have = cureCounterScript.counter.ToString();            //have = current amount of cures
+        counterVsReq.text = have + " / " + need;                //have cured / need cured
+   
+        Debug.Log("Cure more zombies, the door is locked");
+        /*
+         * Change sprite from Up to Down
+         * show image Gingerbreadman
+         * Play sound Gadunk
+        */
+        if (counter <= 0)
+        {
+            requirementCanvas.SetActive(false);
         }
     }
 }
