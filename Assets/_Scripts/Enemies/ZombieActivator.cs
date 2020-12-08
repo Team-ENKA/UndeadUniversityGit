86f0a15f5673d877014dcda36efe1c15197eeeda
Erik Nohr <3345982@student.nord.no>
2020-12-08 20:48:29 +0100

    Full Game Playable!

---
+++ Assets/_Scripts/Enemies/ZombieActivator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieActivator : MonoBehaviour
+{
+    public GameObject zombieGroup;
+    void OnTriggerStay2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            zombieGroup.SetActive(true);
+        }
+
+    }
+}
